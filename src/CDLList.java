8dc762ea6028d97d64284927890db66a35f7e035
Fatemah Almarhoon <fatemahrauf15@gmail.com>
2025-02-28 11:53:49 +0300

    Update CDLList.java
    implementing the required methods
    getLength was renamed to size
    isEmpty was deleted
    the reverse method is not implemented yet

--- src/CDLList.java
+++ src/CDLList.java
@@ -1,4 +1,5 @@
 public class CDLList<T> {
+
     private class DLLNode<T> {
         private T info;
         private DLLNode<T> next, prev;
@@ -14,16 +15,49 @@ public class CDLList<T> {
             this(null, null, null);
         }
     }
+
     protected DLLNode<T> first = null;
     protected DLLNode<T> last = null;
     protected int length = 0;
+
     public CDLList() {
         first = last = null;
     }
-    public int getLength() {
+    public boolean add(T el) {
+        DLLNode<T> n = new DLLNode(el, first, last);
+        return true;
+    }
+    public boolean delete(T el) {
+        DLLNode<T> current = first;
+        while (current != null) {
+            if (current.info.equals(el)) {
+                current.prev.next = current.next;
+                current.next.prev = current.prev;
+                current = null;
+                return true;
+            }
+        }
+        return false;
+    }
+    public T findMiddle() {
+        DLLNode<T> current = first;
+        for (int i = 0; i <= (length-1) / 2; i++) {
+            current = current.next;
+        }
+        return current.info;
+    }
+    // TODO: Complete this method
+    public void reverse() {}
+    public int size() {
         return length;
     }
-    public boolean isEmpty() {
-        return first == null;
+    public void display() {
+        DLLNode<T> current = first;
+        while (current != null) {
+            System.out.print(current.info + " --> ");
+            current = current.next;
+        }
     }
+
+
 }
